a8dcf9fbb4e22c5373b2b2be39a03f376b060e99
Justin Paoletta <justinpaoletta@gmail.com>
2020-04-24 22:38:50 -0400

    server running and accepting form post

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,19 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+const server = express();
+
+server.use(express.static(__dirname + '/client'));
+
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.use(bodyParser.json());
+
+server.post('/csv', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+});
 
 
+
+server.listen(9000, () => {console.log('Im on 9000 come at me')});
